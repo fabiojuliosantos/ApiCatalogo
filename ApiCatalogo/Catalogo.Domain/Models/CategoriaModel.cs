66c71cb58e693e559820596f8feae2fad81700ce
Fábio Júlio <73951977+fabiojuliosantos@users.noreply.github.com>
2024-01-09 10:55:47 -0300

    Atualização do projeto, adicionando models da api

---
+++ ApiCatalogo/Catalogo.Domain/Models/CategoriaModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiCatalogo.Models;
+
+public class CategoriaModel
+{
+    public int CategoriaId { get; set; } //=> Por convencao o EFCore reconhece como chave primaria
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
---
+++ ApiCatalogo/README.md
@@ -0,0 +1,6 @@
+## Estrutura do projeto
+
+|---|---|
+||---|
+|ApiCatalogo| Controllers da API|
+|Catalogo.Domain|Itens de domínio do projeto(Models e afins)|
